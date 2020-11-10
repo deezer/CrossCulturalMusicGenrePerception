cdf0c6367af2701d4fd0dfb13cb0a4aab9bca6dc
eepure <eepure@deezer.com>
2020-11-10 11:09:29 +0100

    fixes

--- ccmgp/dbp_data_collection/step7_select_subgraph_for_sources.py
+++ ccmgp/dbp_data_collection/step7_select_subgraph_for_sources.py
@@ -5,11 +5,16 @@ from ccmgp.utils import utils
 
 graph_file = utils.RAW_GRAPH_PATH
 graph_dir = utils.GRAPH_DIR
+if not os.path.exists(graph_dir):
+    os.mkdir(graph_dir)
+    print("Directory ", graph_dir, " Created ")
+else:
+    print("Directory ", graph_dir, " already exists")
 
 G = nx.read_graphml(graph_file)
 
 lang_pairs = []
-for i in range(len(utils.langs)) - 1:
+for i in range(len(utils.langs) - 1):
     for j in range(i + 1, len(utils.langs)):
         lang_pairs.append([utils.langs[i], utils.langs[j]])
 
@@ -21,7 +26,7 @@ for sources in lang_pairs:
         if lang in sources:
             selected_nodes.append(n)
     newG = G.subgraph(selected_nodes)
-    nx.write_graphml(newG, os.path.join(data_dir, '_'.join(sorted(sources)) + '_graph.graphml'))
+    nx.write_graphml(newG, os.path.join(graph_dir, '_'.join(sorted(sources)) + '_graph.graphml'))
 
     # Second save unaligned graphs for each language pair by removing SameAs links
     ebunch = []
@@ -30,4 +35,4 @@ for sources in lang_pairs:
             ebunch.append((e[0], e[1]))
     newG2 = nx.Graph(newG)
     newG2.remove_edges_from(ebunch)
-    nx.write_graphml(newG2, os.path.join(data_dir, '_'.join(sorted(sources)) + '_graph_unaligned.graphml'))
+    nx.write_graphml(newG2, os.path.join(graph_dir, '_'.join(sorted(sources)) + '_graph_unaligned.graphml'))
